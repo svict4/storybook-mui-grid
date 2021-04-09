0e80e5bef69e9980e187373f71fa4d21bac5625d
Simon <email@simonvictory.com>
2021-04-09 11:56:48 +1000

    return to simple Grid

--- src/components/organisms/broken/Footer.tsx
+++ src/components/organisms/broken/Footer.tsx
@@ -3,15 +3,7 @@ import { Box, Grid, Paper } from "@material-ui/core";
 
 const Footer: React.FC = () => (
   <footer>
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Paper>xs=12</Paper>
-      </Grid>
-    </Grid>
-
-    {/* even this will fail 
-      <Grid>test</Grid>
-    */}
+    <Grid>test</Grid>
   </footer>
 );
 export default Footer;
